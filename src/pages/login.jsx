f18e19a16dd2d823ed16fe4065a07b8c0bfd9eac
Gabriel Yakoub <gabriel.yakooub@gmail.com>
2023-03-27 15:00:54 +0200

    can now fetch playlists

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -4,15 +4,15 @@ import { accessUrl } from "@/config";
 export default function Login() {
     return (
         <div className="py-10 text-center">
-            <h1 className="text-4xl font-bold mb-1">
+            <h1 className="mb-1 text-4xl font-bold">
                 Welcome to my Spotify clone!
             </h1>
-            <p className="text-text-dimmed mb-10">
+            <p className="mb-10 text-text-dimmed">
                 Please Sign in to use the app
             </p>
             <a
                 href={accessUrl}
-                className="py-1.5 px-5 bg-primary rounded-md text-xl font-semibold"
+                className="rounded-md bg-primary py-1.5 px-5 text-xl font-semibold"
             >
                 Sign in
             </a>
