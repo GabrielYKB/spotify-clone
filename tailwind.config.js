76942c5a63b649989beb404cfc8b9400b2c18461
Gabriel Yakoub <gabriel.yakooub@gmail.com>
2023-03-27 11:04:01 +0200

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
     content: ["./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                bg: "#F58",
+                "bg-dimmed": "#121212",
+                text: "#FFFFFF",
+                "text-dimmed": "#b3b3b3",
+                primary: "#2f6",
+            },
+        },
     },
     plugins: [],
 };
