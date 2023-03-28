d1948a68b5854161d359979fabe8e2156423f871
Gabriel Yakoub <gabriel.yakooub@gmail.com>
2023-03-28 13:02:59 +0200

    added layout component and started work on playlist

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -3,6 +3,7 @@ import SpotifyWebApi from "spotify-web-api-node";
 import { redirectURL } from "@/config";
 import { getAccessTokenFromUrl } from "@/utils/getAccessTokenFromUrl";
 import { useRouter } from "next/router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export const spotifyApi = new SpotifyWebApi({
     clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
@@ -10,12 +11,14 @@ export const spotifyApi = new SpotifyWebApi({
     redirectUri: redirectURL,
 });
 
+const queryClient = new QueryClient();
+
 export default function App({ Component, pageProps }) {
     const router = useRouter();
-
     if (typeof window !== "undefined" && router.asPath !== "/login") {
         const accessToken =
             getAccessTokenFromUrl() || sessionStorage.getItem("spotify-key");
+        console.log(accessToken);
 
         if (accessToken) {
             sessionStorage.setItem("spotify-key", accessToken);
@@ -24,5 +27,9 @@ export default function App({ Component, pageProps }) {
             router.replace("/login");
         }
     }
-    return <Component {...pageProps} />;
+    return (
+        <QueryClientProvider client={queryClient}>
+            <Component {...pageProps} />;
+        </QueryClientProvider>
+    );
 }
